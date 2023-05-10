f22a65c991bd5fcd4f7b2123e9bed3fd585da14d
SwethaAppisetty <120455660+SwethaAppisetty@users.noreply.github.com>
2023-05-10 11:28:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favourites')
-streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text(' 🥗Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacado Toast')
