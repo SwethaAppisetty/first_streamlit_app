ff1b81fa394f863f3321d1d2bf02922afb6eb3f3
SwethaAppisetty <120455660+SwethaAppisetty@users.noreply.github.com>
2023-05-10 11:28:45 +0530

    Update streamlit_app.py
    Added a fruit smoothie to breakfast favourites

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text(' 🥗Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
